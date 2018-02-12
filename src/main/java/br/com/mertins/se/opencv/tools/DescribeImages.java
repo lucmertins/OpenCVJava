a3f13eca95a2e97f3c2486c56c13a2c0c76584ec
mertins <luciano@mertins.com.br>
2018-02-12 19:07:09 -0200

    Testando detecção de resenha. Insatisfatório

--- src/main/java/br/com/mertins/se/opencv/tools/DescribeImages.java
+++ src/main/java/br/com/mertins/se/opencv/tools/DescribeImages.java
@@ -1,45 +1,66 @@
 package br.com.mertins.se.opencv.tools;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.imageio.ImageIO;
 
 /**
-Referência
-https://www.youtube.com/watch?v=WEzm7L5zoZE
-http://cogcomp.org/Data/Car/
-
-* Executar codigo java que produz bg.txt e cars.info
-https://github.com/lucmertins/OpenCVJava
-
-Gerar vector
-opencv_createsamples -info cars.info -num 550 -w 48 -h 24 -vec cars.vec
-
-Visualizar
-opencv_createsamples -vec cars.vec -w 48 -h 24
-
-
-opencv_traincascade -data data -vec cars.vec -bg bg.txt -numPos 500 -numNeg 500 -numStages 50 -w 48 -h 24 -featureType LBP
-ou
-opencv_traincascade -data data -vec cars.vec -bg bg.txt -numPos 500 -numNeg 500 -numStages 50 -w 48 -h 24 -featureType HAAR
-
+ * Referência https://www.youtube.com/watch?v=WEzm7L5zoZE
+ * http://cogcomp.org/Data/Car/
+ *
+ * Executar codigo java que produz bg.txt e cars.info
+ * https://github.com/lucmertins/OpenCVJava
+ *
+ * Gerar vector opencv_createsamples -info cars.info -num 550 -w 48 -h 24 -vec
+ * cars.vec
+ *
+ * Visualizar opencv_createsamples -vec cars.vec -w 48 -h 24
+ *
+ *
+ * opencv_traincascade -data data -vec cars.vec -bg bg.txt -numPos 500 -numNeg
+ * 500 -numStages 50 -w 48 -h 24 -featureType LBP ou opencv_traincascade -data
+ * data -vec cars.vec -bg bg.txt -numPos 500 -numNeg 500 -numStages 50 -w 48 -h
+ * 24 -featureType HAAR
+ *
  * @author mertins
  */
 public class DescribeImages {
 
     public void positives() throws IOException {
-        File folder = new File("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingCar/positive");
-        PrintWriter writer = new PrintWriter(new FileWriter("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingCar/cars.info"));
+        File folder = new File("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingResenha/positiveSameSize");
+        PrintWriter writer = new PrintWriter(new FileWriter("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingResenha/resenhas.info"));
+        double minProp = Double.MAX_VALUE;
+        double maxProp = Double.MIN_VALUE;
+
+        int minHeight = Integer.MAX_VALUE;
+        int maxHeight = Integer.MIN_VALUE;
+        int minWidth = Integer.MAX_VALUE;
+        int maxWidth = Integer.MIN_VALUE;
+
         for (File file : folder.listFiles()) {
-            writer.format("%s 1 0 0 100 40\n", file.getAbsoluteFile());
+            BufferedImage bufferedImage = ImageIO.read(file);
+            int width = bufferedImage.getWidth();
+            int height = bufferedImage.getHeight();
+            double prop = (double) width / (double) height;
+            writer.format("%s 1 0 0 %d %d\n", file.getAbsoluteFile(), width, height);
+            minProp = Math.min(minProp, prop);
+            maxProp = Math.max(maxProp, prop);
+            minHeight = Math.min(minHeight, height);
+            maxHeight = Math.max(maxHeight, height);
+            minWidth = Math.min(minWidth, width);
+            maxWidth = Math.max(maxWidth, width);
+
         }
+        System.out.printf("maxProp[%f]  minProp[%f]  minHeight[%d]  maxHeight[%d]  minWidth[%d]  minHeight[%d] \n", maxProp, minProp, minHeight, maxHeight, minWidth, maxWidth);
         writer.close();
     }
 
     public void negatives() throws IOException {
-        File folder = new File("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingCar/negative");
-        PrintWriter writer = new PrintWriter(new FileWriter("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingCar/bg.txt"));
+        File folder = new File("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingResenha/negativeReduzidaQuantSizeMenor");
+        PrintWriter writer = new PrintWriter(new FileWriter("/home/mertins/Documentos/UFPel/Dr/SistemasEvolutivos/OpenCV/TrainingResenha/bg.txt"));
         for (File file : folder.listFiles()) {
             writer.format("%s\n", file.getAbsoluteFile());
         }
--- src/main/java/br/com/mertins/se/testeopencv/TesteTutorial.java
+++ src/main/java/br/com/mertins/se/testeopencv/TesteTutorial.java
@@ -24,19 +24,19 @@ class DetectFaceDemo {
         System.out.println("\nRunning DetectFaceDemo");
         // Create a face detector from the cascade file in the resources
         // directory.
-        CascadeClassifier faceDetector = new CascadeClassifier(getClass().getResource("/car_cascade.xml").getPath());
-        Mat image = Imgcodecs.imread(getClass().getResource("/test-12.pgm").getPath());
+        CascadeClassifier faceDetector = new CascadeClassifier(getClass().getResource("/resenha2_cascade.xml").getPath());
+        Mat image = Imgcodecs.imread(getClass().getResource("/FRONT_59b97f8e3dbc0e5149797ff0_waterfall_positive.png").getPath());
         // Detect faces in the image.
         // MatOfRect is a special container class for Rect.
         MatOfRect faceDetections = new MatOfRect();
         faceDetector.detectMultiScale(image, faceDetections);
-        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+        System.out.println(String.format("Detected %s resenha", faceDetections.toArray().length));
         // Draw a bounding box around each face.
         for (Rect rect : faceDetections.toArray()) {
             Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 255, 0));
         }
         // Save the visualized detection.
-        String filename = "carDetection.png";
+        String filename = "resenhaDetection.png";
         System.out.println(String.format("Writing %s", filename));
         Imgcodecs.imwrite(filename, image);
     }
